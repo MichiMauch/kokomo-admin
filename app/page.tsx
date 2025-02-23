17bb9f5932c401f783c628209e164542b6b87e1a
MichiMauch <michi.mauch@gmail.com>
2025-02-23 19:57:12 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-import { ImageUploadForm } from "@/components/image-upload-form"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { ImageUploadForm } from "@/components/image-upload-form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4 max-w-2xl min-h-screen py-8">
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">KOKOMO House Blogpost erstellen</CardTitle>
+          <CardTitle className="text-2xl">
+            KOKOMO House Blogpost erstellen!
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <ImageUploadForm />
         </CardContent>
       </Card>
     </main>
-  )
+  );
 }
-
